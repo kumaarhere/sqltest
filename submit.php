b1438dff2da37d996b79451d1cbf933457e9b8fa
kumaarkandugula <82639939+kumaarhere@users.noreply.github.com>
2023-06-03 18:02:35 +0530

    Add files via upload

---
+++ submit.php
@@ -0,0 +1,23 @@
+<?php
+ $first_name = $_POST['firstname'];
+ $last_name = $_POST['lastname'];
+ $email = $_POST['email'];
+ $password = $_POST['password'];
+ $gender = $_POST['gender'];
+
+
+//connecting to the database..
+ $conn = mysqli_connect('localhost','root','KUMAAR@9848','form');
+ if ($conn->connect_error){
+    die("Error connecting to the database: " . $conn->connect_error);
+ }else{
+   $stmt=$conn->prepare("insert into `users` (`firstname`, `lastname`, `email`, `password`, `gender`) values(?,?,?,?,?)");
+   $stmt->bind_param('sssss',$first_name,$last_name,$email,$password,$gender);
+   //sssss means here that variables are considered to be "string"
+   $stmt->execute();
+   echo "Registration was successfull.";
+   
+   $stmt->close();
+   $conn->disconnect();
+ }
+?>
